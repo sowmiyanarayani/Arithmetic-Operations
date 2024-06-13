b9568db56cf99b2e5663a7915f01e7927031b799
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-06-13 12:14:14 +0530

    Actions Update

--- src/core/actions.js
+++ src/core/actions.js
@@ -3,8 +3,11 @@ import arithmeticManager from '../services/arithmeticManager';
 const increaseCount = ({ state, data }) => ({ count: state.count + data });
 const setNumberA = ({ data }) => ({ firstNumber: data });
 const setNumberB = ({ data }) => ({ secondNumber: data });
-const setResult = (context) =>
-	({ result: arithmeticManager[context.data](context) });
+const setResult = (context) => {
+	const { data } = context;
+
+	return { result: arithmeticManager[data](context) };
+};
 const actions = { increaseCount, setNumberA, setNumberB, setResult };
 
 export default actions;
